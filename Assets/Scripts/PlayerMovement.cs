c355ef91752052db81facc6ea2fbce1aff140490
Christopher Thai <ChristopherBThai@gmail.com>
2017-04-22 18:38:32 -0700

    Added player jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,24 +7,36 @@ public class PlayerMovement : MonoBehaviour {
 	public float speed;
 
 	private SpriteRenderer sr;
+	private Rigidbody2D rb;
 	private Animator animator;
 
-	private Vector3 vel;
+	private Vector2 vel;
 
 	// Use this for initialization
 	void Start () {
 		sr = GetComponent<SpriteRenderer> ();
 		animator = GetComponent<Animator> ();
-		vel = new Vector3 (speed,0,0);
+		rb = GetComponent<Rigidbody2D> ();
+		vel = new Vector3 (speed,0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		jump ();
 		move ();
+	}
+
+	private void jump(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			vel.x = 0;
+			vel.y = 1000;
+			rb.AddForce (vel);
+		}
 	}
 
 	private void move(){
 		vel.x = 0;
+		vel.y = 0;
 		if (Input.GetKey (KeyCode.D)) {
 			sr.flipX = false;
 			vel.x = speed;
