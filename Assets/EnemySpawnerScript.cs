525e9cb70622d39359ef1070b05f05edf255e616
Christopher Thai <ChristopherBThai@gmail.com>
2017-04-23 02:04:29 -0700

    Created an enemy spawner and spawnpoints!

---
+++ Assets/EnemySpawnerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour {
+
+	public GameObject target;
+	public float spawnInterval;
+	public Transform[] groundSpawnPoint,flyingSpawnPoint;
+	public GameObject[] groundEnemies,flyingEnemies;
+
+	private float timer;
+
+
+	// Use this for initialization
+	void Start () {
+		timer = spawnInterval;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 0) {
+			timer -= Time.deltaTime;
+			if (timer <= 0) {
+				spawnRandomEnemy ();
+				timer = Random.Range (1, spawnInterval);
+			}
+		}
+	}
+
+	void spawnRandomEnemy(){
+		if (Random.value > .5) { //Ground
+			GameObject enemy = Instantiate(groundEnemies[(int)Random.Range(0,groundEnemies.Length)]);
+			enemy.transform.position = (groundSpawnPoint[(int)Random.Range(0,groundSpawnPoint.Length)].transform.position);
+			enemy.GetComponent<EnemyMove>().setTarget (target);
+		} else { //Flying
+			GameObject enemy = Instantiate(flyingEnemies[(int)Random.Range(0,flyingEnemies.Length)]);
+			enemy.transform.position = (flyingSpawnPoint[(int)Random.Range(0,flyingSpawnPoint.Length)].transform.position);
+			enemy.GetComponent<EnemyMove>().setTarget (target);
+		}
+	}
+}
--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -8,37 +8,54 @@ public class EnemyMove : MonoBehaviour {
 	public GameObject target;
 	private Vector3 vel;
 	private bool dead;
+	private Animator animator;
+	private SpriteRenderer sr;
 
 	// Use this for initialization
 	void Start () {
 		vel = new Vector3 ();
 		dead = false;
+		animator = GetComponent<Animator> ();
+		sr = GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(!dead)
 			move ();
+		if (transform.position.y < -10)
+			die ();
 	}
 
 	void move(){
 		vel.x = 0;
 		float pPosition = target.transform.position.x;
 		if (pPosition > transform.position.x) {
+			sr.flipX = false;
 			vel.x = speed;
 		} else {
 			vel.x = -speed;
+			sr.flipX = true;
 		}
 		transform.Translate (vel);
+	}
+
+	public void setTarget(GameObject nTarget){
+		target = nTarget;
 	}
 
 	public void die(){
 		dead = true;
 		GetComponent<Rigidbody2D> ().gravityScale = 0;
 		GetComponent<BoxCollider2D> ().isTrigger = true;
+		animator.SetTrigger ("EnemyExplode");
 	}
 
 	public bool isDead(){
 		return dead;
+	}
+
+	public void destroy(){
+		Destroy (gameObject);
 	}
 }
